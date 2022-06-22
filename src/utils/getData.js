2a2ca6208a55a818b5d813e91c4dbc110f599af9
Miguel Hernández <miguelhernandezk@gmail.com>
2022-06-22 01:40:55 -0500

    Lessons 15 and 17 completed

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,4 +1,4 @@
-const API = 'https://randomuser.me/api/';
+const API = process.env.API // It is read from the .env file
 
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 const DotEnv = require("dotenv-webpack");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -14,6 +11,7 @@ module.exports = {
         filename: "[name].[contenthash].js",
         assetModuleFilename: "assets/images/[hash][ext][query]",
     },
+    mode: "development", // for development purposes. The JSON was modified as well
     resolve: {
         extensions: [".js"],
         alias: {
@@ -67,13 +65,6 @@ module.exports = {
             ]
         }),
         new DotEnv(),
-        new CleanWebpackPlugin(),
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
-    }
+    // Optimization is deleted in dev mode since it is not required
 }
